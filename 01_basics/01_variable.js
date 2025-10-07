60e45ec501471acb4c93d4607508c0822c6a5dfb
avanishrai19 <raiavanish657@gmail.com>
2025-10-07 17:18:19 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+ const acountID = 14454;
+ let acountEmail = "raiavanish453@gmail.com";
+ var acountPass = "12345";
+ acountCity = "Azamgarh";
+
+ //conts variable value does not change
+ //acountId=25552 NOT ALLOWED
+ console.log(acountID);
+ acountEmail = "raimaneesh@123.com";
+ acountPass = "5554";
+ acountCity="Dehradun";
+  console.table([acountID, acountEmail, acountPass,acountCity]);
